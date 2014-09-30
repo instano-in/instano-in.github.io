d8133e7a17a08b612a5337a5d20e8f41d91df076
Vedant Agarwala <vedant.kota@gmail.com>
2014-09-30 18:48:52 +0530

    Improved maps functionality, showing a big instano logo 	new file:   CNAME

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,14 +41,14 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(12.9442415,77.6275123), // Ghatprabha block, national games village
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
+//         scrollwheel: false,
+//         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -160,7 +160,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
@@ -168,8 +168,9 @@ function init() {
     var map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    // TODO: fetch LatLng from instano-api, parse JSON and display
+    var image = 'img/instano_launcher.png';
+    var myLatLng = new google.maps.LatLng(12.9442415, 77.6275123);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
